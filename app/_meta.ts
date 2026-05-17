a98031035b2b00d37d1bf82a17a17eb3da3229a1
Min Thu Khant <minthukhant.it@gmail.com>
2026-05-17 08:56:22 +0000

    ts complete

---
+++ app/_meta.ts
@@ -0,0 +1,5 @@
+export default {
+    index: "Introduction",
+    javascript: "JavaScript",
+    typescript: "TypeScript",
+}
---
+++ app/typescript/_meta.ts
@@ -0,0 +1,8 @@
+export default {
+    index: "Overview",
+    types: "Data Types",
+    "type-aliases": "Type Aliases",
+    interfaces: "Interfaces",
+    generics: "Generics",
+    functions: "Functions",
+}
